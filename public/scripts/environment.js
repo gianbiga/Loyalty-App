206cacb242c995153ae3f58eb7d564c4f9a60de8
gianbiga <gian.biga@gmail.com>
2019-03-22 14:37:36 -0300

    'zbia'

--- public/scripts/environment.js
+++ public/scripts/environment.js
@@ -20,17 +20,17 @@ var NBO = "NBO_c";
 
 
 /*IDs das Medalhas*/
-var prod_1_1 = "Badge_Primeiros_Passos_c";
-var prod_1_2 = "Badge_Convide_Amigo_c";
+var prod_1_1 = "BadgePrimeirosPassos_c";
+var prod_1_2 = "BadgeConvideAmigo_c";
 var prod_1_3 = "collaboration";
 var prod_2_1 = "congrats";
 var prod_2_2 = "different";
-var prod_2_3 = "Badge_Familia_c";
+var prod_2_3 = "BadgeFamilia_c";
 var prod_3_1 = "furniture";
 var prod_3_2 = "popular";
 var prod_3_3 = "hiker";
 var prod_3_4 = "Badge_Lover_c";
-var prod_4_1 = "Badge_Doacao_c";
+var prod_4_1 = "BadgeDoacao_c";
 var prod_4_2 = "service";
 var prod_4_3 = "expert";
 
