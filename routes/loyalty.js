44910aeea209a11a9559a6e805db4a18615ea5b1
gianbiga <gian.biga@gmail.com>
2019-04-16 11:19:04 -0300

    'cors'

--- routes/loyalty.js
+++ routes/loyalty.js
@@ -13,18 +13,12 @@ module.exports = function (app) {
 	app.use(bodyParser.urlencoded({ extended: true }));
 
 
-	var whitelist = ['https://coalitionappgb1.herokuapp.com', 'http://example2.com'];
-	var corsOptionsDelegate = function (req, callback) {
-	  var corsOptions;
-	  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-	    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-	  } else {
-	    corsOptions = { origin: false } // disable CORS for this request
-	  }
-	  callback(null, corsOptions) // callback expects two parameters: error and options
+	var corsOptions = {
+	  origin: 'http://example.com',
+	  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 	}
 
-	app.use(cors(corsOptionsDelegate));
+	app.use(cors(corsOptions));
 
 	//Create Base Path for Loyalty Calls
 	var loyaltyInstance = axios.create({
