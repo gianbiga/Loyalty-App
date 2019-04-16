d64bbbaaed6fe0842603dfd68b32ce17c705de66
gianbiga <gian.biga@gmail.com>
2019-04-16 10:57:22 -0300

    'cors'

--- routes/loyalty.js
+++ routes/loyalty.js
@@ -11,7 +11,20 @@ module.exports = function (app) {
 	//O body parser é necessário para fazer os POSTs
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(cors());
+
+
+	var whitelist = ['https://coalitionappgb.herokuapap.com', 'http://example2.com']
+	var corsOptions = {
+	  origin: function (origin, callback) {
+	    if (whitelist.indexOf(origin) !== -1) {
+	      callback(null, true)
+	    } else {
+	      callback(new Error('Not allowed by CORS'))
+	    }
+	  }
+	}
+
+	app.use(cors(corsOptions));
 
 	//Create Base Path for Loyalty Calls
 	var loyaltyInstance = axios.create({
